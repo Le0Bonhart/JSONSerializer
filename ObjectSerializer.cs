918b7bced51bf441adb0ad68ae9ea7a3bcf64884
Le0Bonhart <Le0Bonhart@yandex.ru>
2023-05-30 21:51:54 +1000

    Serialization

--- ObjectSerializer.cs
+++ ObjectSerializer.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace JSONSerializer
 {
     public static partial class Serializer
     {
+        public static string Serialize(object obj)
+        {
+            string serialized = "{\n";
+            serialized = $"{serialized} \"class\" : \"{obj.GetType()}\", \n";
+            FieldInfo[] fields = obj.GetType().GetFields();
+             foreach (FieldInfo field in fields)
+            {
+                dynamic val = field.GetValue(obj);
+                serialized += $"\"{field.Name}\" : {Serialize(val)}, \n";
+            }
+            serialized = serialized.Remove(serialized.Length - 3);
+            serialized += "\n}";
 
+            return serialized;
+        }
     }
 }
